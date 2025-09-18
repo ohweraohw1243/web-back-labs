547cf8e5892fc9e5df4d6feecae0eb8ebffe6c63
Волков Даниил <v01k0v.d4ny@gmail.com>
2025-09-18 16:58:04 +0700

    Служебная информация

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
+import datetime
 app = Flask(__name__)
 
 @app.route("/")
@@ -46,11 +47,18 @@ count = 0
 def counter():
     global count
     count +=1
+    time = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+    url = request.url
+    client_ip = request.remote_addr    
     return '''
 <!doctype html>
 <html>
     <body>
         Сколько раз вы сюда заходили: ''' + str(count) + '''
+        <hr>
+        Дата и время: ''' + time + ''' <br>
+        Запрошенный адрес: ''' + url + ''' <br>
+        Ваш IP-адрес: ''' + client_ip + ''' <br>
     </body>
 </html>
 '''
