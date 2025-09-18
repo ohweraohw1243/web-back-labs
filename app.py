580cfde80b475e79c387ec5b0f6ca2a58b99dd69
Волков Даниил <v01k0v.d4ny@gmail.com>
2025-09-18 18:05:37 +0700

    Коды ответов HTTP

--- app.py
+++ app.py
@@ -135,3 +135,81 @@ def created():
     </body>
 </html>
 ''',201
+
+@app.route("/400")
+def bad_request():
+    return '''<!doctype html>
+<html>
+<head>
+    <title>400 Bad Request</title>
+</head>
+<body>
+    <h1>400 Bad Request</h1>
+    <p>Сервер не может обработать запрос из-за синтаксической ошибки.</p>
+</body>
+</html>''', 400
+
+@app.route("/401")
+def unauthorized():
+    return '''<!doctype html>
+<html>
+<head>
+    <title>401 Unauthorized</title>
+</head>
+<body>
+    <h1>401 Unauthorized</h1>
+    <p>Для доступа к запрашиваемому ресурсу требуется аутентификация.</p>
+</body>
+</html>''', 401
+
+@app.route("/402")
+def payment_required():
+    return '''<!doctype html>
+<html>
+<head>
+    <title>402 Payment Required</title>
+</head>
+<body>
+    <h1>402 Payment Required</h1>
+    <p>Для доступа к ресурсу требуется оплата.</p>
+</body>
+</html>''', 402
+
+@app.route("/403")
+def forbidden():
+    return '''<!doctype html>
+<html>
+<head>
+    <title>403 Forbidden</title>
+</head>
+<body>
+    <h1>403 Forbidden</h1>
+    <p>Доступ к ресурсу запрещен.</p>
+</body>
+</html>''', 403
+
+@app.route("/405")
+def method_not_allowed():
+    return '''<!doctype html>
+<html>
+<head>
+    <title>405 Method Not Allowed</title>
+</head>
+<body>
+    <h1>405 Method Not Allowed</h1>
+    <p>Метод нельзя применить для данного ресурса.</p>
+</body>
+</html>''', 405
+
+@app.route("/418")
+def teapot():
+    return '''<!doctype html>
+<html>
+<head>
+    <title>418 I'm a teapot</title>
+</head>
+<body>
+    <h1>418 I'm a teapot</h1>
+    <p>Я чайник и не могу заваривать кофе.</p>
+</body>
+</html>''', 418
