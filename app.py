bcc4a6cc6527b6ca196ce219d2c19c12cb7433f3
Даниил Волков <v01k0v.d4ny@gmail.com>
2025-09-20 11:02:47 +0700

    Заготовка шаблонов

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, redirect, abort
+from flask import Flask, url_for, request, redirect, abort, render_template
 import datetime
 app = Flask(__name__)
 
@@ -338,4 +338,8 @@ def add_flower(name):
     <p>Полный список: {flower_list}</p>
     </body> 
 </html>   
-'''       
+'''      
+
+@app.route('/lab2/example')
+def example():
+    return render_template('example.html')
