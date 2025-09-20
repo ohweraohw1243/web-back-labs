10dc3379ef043fded779b550545ba39424326e2e
Даниил Волков <v01k0v.d4ny@gmail.com>
2025-09-20 11:05:10 +0700

    Имя студента

--- app.py
+++ app.py
@@ -342,4 +342,5 @@ def add_flower(name):
 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Даниил Волков'
+    return render_template('example.html', name=name)
