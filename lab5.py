c038d7c44bb66545dee835d71e61cb4cc9df2edb
Даниил Волков <v01k0v.d4ny@gmail.com>
2025-11-01 09:32:27 +0700

    Заготовка к 5-ой лабораторной

---
+++ lab5.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+
+lab5 = Blueprint('lab5', __name__)
+
+@lab5.route('/lab5/')
+def lab():
+    name =  'Anonymous'
+    return render_template('lab5/lab5.html', name=name)
