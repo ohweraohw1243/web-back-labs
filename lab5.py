e6b20a1337051e646726d1650a176599c3ea124a
Даниил Волков <v01k0v.d4ny@gmail.com>
2025-11-01 11:35:27 +0700

    Правки

--- lab5.py
+++ lab5.py
@@ -47,12 +47,12 @@ def register():
     
     conn, cur = db_connect()
     
-    cur.execute(f"SELECT login FROM users WHERE login='{login}';")
+    cur.execute("SELECT login FROM users WHERE login=%s;", (login, ))
     if cur.fetchone():
         db_close(conn, cur)
         return render_template('lab5/register.html', error="Такой пользователь уже существует")
     password_hash = generate_password_hash(password)
-    cur.execute(f"INSERT INTO users (login,password) VALUES ('{login}', '{password_hash}');")
+    cur.execute("INSERT INTO users (login,password) VALUES (%s, %s);", (login, password))
     db_close(conn, cur)
     return render_template('lab5/success.html', login=login)    
 
