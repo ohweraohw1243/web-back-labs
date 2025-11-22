eeead41e57cc1d6d5b98c5e0114bd74115697b50
Волков Даниил <v01k0v.d4ny@gmail.com>
2025-11-23 01:19:35 +0700

    Снятие аренды

--- lab6.py
+++ lab6.py
@@ -7,10 +7,10 @@ for i in range(1, 11):
     offices.append({"number": i, "tenant": ""})
 
 @lab6.route('/lab6/')
-def lab():
+def main():
     return render_template('lab6/lab6.html')
 
-@lab6.route('/lab6/json-rpc-api/', methods = ['POST'])
+@lab6.route('/lab6/json-rpc-api', methods = ['POST'])
 def api():
     data = request.json
     id = data['id']
@@ -46,6 +46,35 @@ def api():
                         'id': id
                     }
                 office['tenant'] = login
+                return {
+                    'jsonrpc': '2.0',
+                    'result': 'success',
+                    'id': id
+                }
+
+    if data['method'] == 'cancellation':
+        office_number = data['params']
+        for office in offices:
+            if office['number'] == office_number:
+                if not office['tenant']:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 3,
+                            'message': 'Office not booked'
+                        },
+                        'id': id
+                    }
+                if office['tenant'] != login:
+                    return {
+                        'jsonrpc': '2.0',
+                        'error': {
+                            'code': 4,
+                            'message': 'Not your booking'
+                        },
+                        'id': id
+                    }
+                office['tenant'] = ""
                 return {
                     'jsonrpc': '2.0',
                     'result': 'success',
