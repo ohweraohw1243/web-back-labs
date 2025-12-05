2c91735c68aa6956184563c7e7595a27be466183
Волков Даниил <v01k0v.d4ny@gmail.com>
2025-12-05 22:13:18 +0700

    самостоятельное задание - логин после регистрации

--- lab8.py
+++ lab8.py
@@ -11,7 +11,7 @@ lab8 = Blueprint('lab8', __name__)
 def lab():
     return render_template('lab8/lab8.html')
 
-@lab8.route('/lab8/register/', methods = ['GET', 'POST'])
+@lab8.route('/lab8/register/', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
         return render_template('lab8/register.html')
@@ -27,14 +27,19 @@ def register():
         return render_template('lab8/register.html',
                                error='Пароль не должен быть пустым')
     
-    login_exists = users.query.filter_by(login = login_form).first()
+    login_exists = users.query.filter_by(login=login_form).first()
     if login_exists:
         return render_template('lab8/register.html',
-                                error = 'Такой пользователь уже существует')
+                               error='Такой пользователь уже существует')
+
     password_hash = generate_password_hash(password_form)
-    new_user = users(login = login_form, password = password_hash)
+    new_user = users(login=login_form, password=password_hash)
+
     db.session.add(new_user)
     db.session.commit()
+
+    login_user(new_user, remember=False)
+
     return redirect('/lab8/')
 
 
