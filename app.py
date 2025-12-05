53be7da88a1ab58b72a9969712622bd7e752405c
Волков Даниил <v01k0v.d4ny@gmail.com>
2025-12-05 18:48:17 +0700

    заготовка к 8-ой лабораторной

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
+from lab8 import lab8
 from rgz import rgz
 import os
 
@@ -22,6 +23,7 @@ app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
+app.register_blueprint(lab8)
 
 app.register_blueprint(rgz)
 
@@ -44,6 +46,7 @@ def index():
                 <li><a href="/lab5">Пятая лабораторная</a></li>
                 <li><a href="/lab6">Шестая лабораторная</a></li>
                 <li><a href="/lab7">Седьмая лабораторная</a></li>
+                <li><a href="/lab8">Восьмая лабораторная</a></li>
                 <li><a href="/rgz/">РГЗ — Камера хранения</a></li>
             </ul>
         </nav>
