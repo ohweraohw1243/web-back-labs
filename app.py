20b0df9cbf5942bdb09766ddf2a5da96f29bd993
Волков Даниил <v01k0v.d4ny@gmail.com>
2025-12-05 20:29:07 +0700

    правки

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ if app.config['DB_TYPE'] == 'postgres':
 else:
   dir_path = path.dirname(path.realpath(__file__))
   db_path = path.join(dir_path, "daniil_volkov_orm.db")
-  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite://{db_path}'
+  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 
 db.init_app(app)
 
